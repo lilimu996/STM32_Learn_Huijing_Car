50147d8b4257b86d26d1a60d5375495cb4ea3fa8
junhaolics <junhaolics@foxmail.com>
2023-07-11 16:56:11 +0800

    蜂鸣器实验

---
+++ 2 蜂鸣器实验/User/main.c
@@ -0,0 +1,27 @@
+#include "stm32f10x.h"
+#include "interface.h"
+
+int main() //你好，星慈光零基础STM32编程视频教程
+{
+    GPIOCLKInit();              /*初始化GPIO时钟*/
+    UserLEDInit();              /*初始化LED灯*/
+    delay_init();               /*初始化延时函数*/
+    FMInit();                  /*初始化蜂鸣器函数*/
+	while(1)
+	{
+        
+        
+          FM_RESET;               /*打开蜂鸣器*/
+          Delayms(500);           /*延时200ms*/
+          FM_SET;                /*关闭蜂鸣器*/
+          Delayms(900);           /*延时900ms*/
+//		  LED8_RESET;              /*点亮LED8灯*/
+//        Delayms(200);           /*延时200ms*/
+//        LED8_SET;               /*熄灭LED8灯*/
+//        LED_RESET;              /*点亮LED灯*/
+//        Delayms(200);           /*延时200ms*/
+//        LED_SET;               /*熄灭LED灯*/
+//        Delayms(200);           /*延时200ms*/
+        
+	}
+}
